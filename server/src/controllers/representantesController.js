67c707098c4964e69d7488c73f74d21b31343ac9
Omar Sanchez Cuevas <OmR_Work@hotmail.com>
2024-03-07 17:13:00 -0400

    Read operation done

--- server/src/controllers/representantesController.js
+++ server/src/controllers/representantesController.js
@@ -1,8 +1,32 @@
-const { prisma } = require("../service/prisma");
+const { prisma } = require("../service/prisma.js");
 
-const getRepresentantes = async (req, res) => {};
+const getRepresentantes = async (req, res, next) => {
+  try {
+    const response = await prisma.representantes.findMany();
+    res.status(200).json({
+      status: "OK",
+      data: { representantes: response },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-const getRepresentante = async (req, res) => {};
+const getRepresentante = async (req, res, next) => {
+  try {
+    const response = await prisma.representantes.findUniqueOrThrow({
+      where: {
+        Representante_id: parseInt(req.params.id),
+      },
+    });
+    res.status(200).json({
+      status: "OK",
+      data: { representante: response },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const postRepresentante = async (req, res) => {};
 
