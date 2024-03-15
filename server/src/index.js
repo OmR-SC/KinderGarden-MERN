caaebb7df546baaf591ee92778ae5d2b6232f04b
Omar Sanchez Cuevas <OmR_Work@hotmail.com>
2024-03-15 19:40:02 -0400

    Added endpoints documentation by Swagger

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const { errorHandlingAfterRoute } = require("./middlewares/error-handler.js");
 
 const reprsentantesRoutes = require("./v1/routes/representantesRoutes.js");
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger.js");
 
 const app = express();
 
@@ -13,11 +14,19 @@ app.use(express.json());
 
 //Routing
 app.use("/api/v1", reprsentantesRoutes);
-app.get("/", (req, res) => res.send("Hello world 👋"));
+app.get("/", (req, res) =>
+  res.send(`Hello world 👋 \n
+Swagger documentation at =>  <a href="${
+    req.protocol + "://" + req.get("host") + "/api/v1/docs"
+  }">Swagger</a>`)
+);
 
 //Middlewares
 app.use(errorHandlingAfterRoute);
 
 //Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`The server is listening on PORT #${PORT}`));
+app.listen(PORT, () => {
+  console.log(`The server is listening on PORT #${PORT}`);
+  V1SwaggerDocs(app, PORT);
+});
---
+++ server/src/v1/swagger.js
@@ -0,0 +1,44 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "My Kindergarten API",
+    version: "1.0.0",
+    description: "A kingergarten API",
+  },
+  contact: {
+    name: "Omar",
+    email: "OmR_Work@hotmail.com",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api/v1",
+      description: "Development server",
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ["./src/v1/routes/representantesRoutes.js", "./src/schemas/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+//SwaggerUi
+
+const swaggerDocs = (app, port) => {
+  app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/api/v1/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+
+  console.log(
+    `Version 1 Docs are available on http://localhost:${port}/api/v1/docs`
+  );
+};
+
+module.exports = { swaggerDocs };
